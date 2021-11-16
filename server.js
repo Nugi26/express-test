77bb6916134f8df8370ed588f9f0255852debbff
nugi26 <m.adinugraha.ug@gmail.com>
2021-11-16 15:44:02 +0700

    change port

--- server.js
+++ server.js
@@ -2,12 +2,12 @@ const { application } = require('express');
 const express = require('express');
 const cors = require('cors');
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.get('/', (req, res) => {
-  res.json({message: 'haloooo'})
+  res.json({ message: 'haloooo' });
 });
 
 app.listen({ port }, () => console.log(`Server is running at ${port}`));
